c09530a08d42d4eb5ef5c4b89c204090934649f8
Jackson Spindle <jacksonmilesspindle@gmail.com>
2022-04-07 14:44:14 -0400

    added CSS styling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React, { useEffect } from "react"
 import Book from './components/Book'
+// import BookViewer from "./components/BookViewer"
 import './style.css'
 // import { axios } from 'axios'
 import $ from 'jquery'
+
 
 
 export default function App(){
 
 
 // STATES
-    const [searchBarValue, setSearchBarValue] = React.useState("drive")
+    const [searchBarValue, setSearchBarValue] = React.useState("The Singularity is Near")
     const [bookSearch, setBookSearch] = React.useState("")
     const [book, setBookData] = React.useState({
         bookWidth: 2,
         bookName: "",
         cover: "./components/instagramLogo.png",
+        title: "",
+        author: "",
+        description: "",
+        previewLink: ""
     })
+
+    console.log(book.author)
 
 
 // API CALL    
@@ -27,15 +35,27 @@ export default function App(){
                 const response = await fetch(googleAPI);
                 const googleBooksData = await response.json();
 
-                // console.log(googleBooksData.items[0].volumeInfo.pageCount);
+                console.log(googleBooksData.items[0].volumeInfo);
+
+
+
+
                 setBookData(prevState => {
+                    
                     return {
                         ...book,
                         bookWidth: googleBooksData.items[0].volumeInfo.pageCount / 200,
                         bookName:  googleBooksData.items[0].volumeInfo,
-                        cover:   googleBooksData.items[0].volumeInfo.imageLinks.thumbnail
+                        cover:   googleBooksData.items[0].volumeInfo.imageLinks.thumbnail,
+                        author: googleBooksData.items[0].volumeInfo.authors, 
+                        description: googleBooksData.items[0].volumeInfo.description,
+                        title: googleBooksData.items[0].volumeInfo.title,
+                        previewLink: googleBooksData.items[0].volumeInfo.previewLink
                         }
                     })
+
+
+                    
 
 
                 // console.log(googleBooksData.items[0].volumeInfo.imageLinks.thumbnail)
@@ -67,26 +87,35 @@ export default function App(){
 
 
     return (
-        <div>
+        <div className="mainDiv">
+            <div  className="sceneCanvas">
             <Book width={book.bookWidth} cover={book.cover}/>
+            </div>
+            <div className="bookCard">
             {/* <Searchbar value={bookSearch} handleChange={searchBarValue}/> */}
-            <input id="search" ></input>
-            <button onClick={()=>{
-                let searchInput = document.getElementById('search')
-                // console.log(searchInput.value)
-                // setSearchBarValue(searchInput.value)
-                // console.log(searchInput.value)
-                setSearchBarValue(searchInput.value)
-                // console.log(book.bookWidth)
-                
+                <input id="search" autoComplete="off"></input>
+                <button onClick={()=>{
+                    let searchInput = document.getElementById('search')
+                    // console.log(searchInput.value)
+                    // setSearchBarValue(searchInput.value)
+                    // console.log(searchInput.value)
+                    setSearchBarValue(searchInput.value)
+                    // console.log(book.bookWidth)
+                    
 
 
 
-                
-                // console.log(searchBarValue)
-                // console.log(book)
-            }}></button>
-            <img src={book.cover} cover={book.cover}></img>
+                    
+                    // console.log(searchBarValue)
+                    // console.log(book)
+                }}>SEARCH BOOK</button>
+                <h1>{book.title}</h1>
+                <h2>{book.author}</h2>
+                <img src={book.cover} cover={book.cover}></img>
+                <p>{book.description}</p>
+                {/* <BookViewer /> */}
+            </div>
+            
         </div>
     )
 }
